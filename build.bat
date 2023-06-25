ea9fa54ab952336df0bab68d7bcc5e0e73d10b55
minhgalaxy <vvminh198@gmail.com>
2023-06-25 22:01:02 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-node node_modules\react-native\cli.js bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output index.android.bundle --assets-dest res --sourcemap-output maps\index.android.bundle.packager.map --minify false --verbose
+node node_modules\react-native\cli.js bundle --platform android --dev false --reset-cache --entry-file index.js --bundle-output index.android.bundle.js --assets-dest res --sourcemap-output output\sourcemaps\index.android.bundle.packager.map --minify false --verbose
 
-node_modules\react-native\sdks\hermesc\win64-bin\hermesc.exe -emit-binary -max-diagnostic-width=80 -out index.android.bundle.hbc index.android.bundle -O -output-source-map
+node_modules\react-native\sdks\hermesc\win64-bin\hermesc.exe -emit-binary -max-diagnostic-width=80 -out index.android.bundle.hbc index.android.bundle.js -O -output-source-map
